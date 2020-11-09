857c617007e386f933a5b60032c80ad4abb4fc64
fjsuarez <fjsuarez@gmail.com>
2020-11-09 11:50:46 -0400

    Script de codigo morse

---
+++ morse.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+. /lib/ramips.sh
+
+$tabla=$(ramips_board_name)
+
+_MorseMain () {
+    echo morse > /sys/class/leds/$tabla:\amber:\system/trigger
+    echo 120 > /sys/class/leds/$tabla:\amber:\system/delay
+    echo $* > /sys/class/leds/$tabla:\amber:\system/message
+}
+
+_MorseMain $*
+
+exit
