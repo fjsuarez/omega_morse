52d16b74b8c1d8c997b44d9158b672248fcf336b
fjsuarez <fjsuarez@gmail.com>
2020-11-09 12:13:06 -0400

    Errores de typeo

--- morse.sh
+++ morse.sh
@@ -2,12 +2,12 @@
 
 . /lib/ramips.sh
 
-$tabla=$(ramips_board_name)
+tabla=$(ramips_board_name)
 
 _MorseMain () {
-    echo morse > /sys/class/leds/$tabla:\amber:\system/trigger
-    echo 120 > /sys/class/leds/$tabla:\amber:\system/delay
-    echo $* > /sys/class/leds/$tabla:\amber:\system/message
+    echo morse > /sys/class/leds/$tabla\:amber\:system/trigger
+    echo 120 > /sys/class/leds/$tabla\:amber\:system/delay
+    echo $* > /sys/class/leds/$tabla\:amber\:system/message
 }
 
 _MorseMain $*
